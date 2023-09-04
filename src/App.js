335d6bd5f426efa2b3f786a7faae2635a4654034
Dylan Lee <leedylan0918@gmail.com>
2023-09-05 01:12:48 +0800

    Added a add friend function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './shared/component/Button';
 import FriendsList from './components/FriendsList';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FormSplitBill';
 
+const initialFriends = [
+    {
+        id: 118836,
+        name: 'Clark',
+        image: 'https://i.pravatar.cc/48?u=118836',
+        balance: -7,
+    },
+    {
+        id: 933372,
+        name: 'Sarah',
+        image: 'https://i.pravatar.cc/48?u=933372',
+        balance: 20,
+    },
+    {
+        id: 499476,
+        name: 'Anthony',
+        image: 'https://i.pravatar.cc/48?u=499476',
+        balance: 0,
+    },
+];
+
 const App = () => {
+    const [friends, setFriends] = useState(initialFriends);
+    const [showAddFriend, setShowAddFriend] = useState(false);
+
+    // Show Add Friend Form
+    const handleShowAddFriend = () => {
+        setShowAddFriend((show) => !show);
+    };
+
+    // Callback Function
+    const handleAddFriend = (friend) => {
+        setFriends((friends) => [...friends, friend]);
+        setShowAddFriend(false);
+    };
+
     return (
         <>
             <div className='app'>
                 <div className='sidebar'>
-                    <FriendsList />
-                    <FormAddFriend />
-                    <Button>Add Friend</Button>
+                    <FriendsList friends={friends} />
+                    {showAddFriend && (
+                        <FormAddFriend onAddFriend={handleAddFriend} />
+                    )}
+
+                    <Button onClick={handleShowAddFriend}>
+                        {showAddFriend ? 'Close' : 'Add Friend'}
+                    </Button>
                 </div>
 
                 <FormSplitBill />
--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../shared/component/Button';
 
-const FormAddFriend = () => {
+const FormAddFriend = ({ onAddFriend }) => {
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name || !image) return;
+
+        const id = crypto.randomUUID();
+        const newFriend = {
+            id,
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+        };
+
+        // Pass data in parent component
+        onAddFriend(newFriend);
+
+        // reset value
+        setName('');
+        setImage('https://i.pravatar.cc/48');
+    };
+
     return (
         <>
-            <form className='form-add-friend'>
+            <form className='form-add-friend' onSubmit={handleSubmit}>
                 <label>🧑‍🤝‍🧑Friend Name</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
 
                 <label>🌇 Image URL</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={image}
+                    onChange={(e) => setImage(e.target.values)}
+                />
 
                 <Button>Add</Button>
             </form>
